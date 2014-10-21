c9f332f2694923f3717648b898592d6a2719a6b7
CLCL <cloono+github@gmail.com>
2014-10-22 02:23:49 +0900

    make UTILITIES.cpp/UTILITIES.h and move adjustCompiledTime

---
+++ UTILITIES.h
@@ -0,0 +1,19 @@
+#ifndef UTILITIES_H_
+#define UTILITIES_H_
+#include "arduino.h"
+#include <Time.h>
+
+// クラスの定義
+class UTILITIES
+{
+public:
+  UTILITIES(void);   // コンストラクタ
+  time_t adjustCompiledTime(void);
+  bool getTime(const char *str);
+  bool getDate(const char *str);
+private:
+  tmElements_t tm; 
+};
+
+#endif
+
